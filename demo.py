307cde27b40407fefa195c72680f9ffab1eb98ea
Larry Myerscough <hippostech@gmail.com>
2018-11-06 20:12:49 +0100

    easy changes for qt5

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt4 import QtGui
-import popplerqt4
+from PyQt5 import QtGui, QtWidgets
+import popplerqt5
 
 usage = """
 Demo to load a PDF and display the first page.
@@ -15,26 +15,26 @@ Usage:
 def pdf_view(filename):
     """Return a Scrollarea showing the first page of the specified PDF file."""
     
-    label = QtGui.QLabel()
+    label = QtWidgets.QLabel()
     
-    doc = popplerqt4.Poppler.Document.load(filename)
-    doc.setRenderHint(popplerqt4.Poppler.Document.Antialiasing)
-    doc.setRenderHint(popplerqt4.Poppler.Document.TextAntialiasing)
+    doc = popplerqt5.Poppler.Document.load(filename)
+    doc.setRenderHint(popplerqt5.Poppler.Document.Antialiasing)
+    doc.setRenderHint(popplerqt5.Poppler.Document.TextAntialiasing)
     
     page = doc.page(0)
     image = page.renderToImage()
     
     label.setPixmap(QtGui.QPixmap.fromImage(image))
     
-    area = QtGui.QScrollArea()
+    area = QtWidgets.QScrollArea()
     area.setWidget(label)
     area.setWindowTitle(filename)
     return area
 
 
 def main():
-    app = QtGui.QApplication(sys.argv)
-    argv = QtGui.QApplication.arguments()
+    app = QtWidgets.QApplication(sys.argv)
+    argv = QtWidgets.QApplication.arguments()
     if len(argv) < 2:
         sys.stderr.write(usage)
         sys.exit(2)
